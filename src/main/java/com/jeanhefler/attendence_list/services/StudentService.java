33215ce3675676ce8df99fce6b78e55faf14edb2
Jeanhefler <jeanhefler@outlook.com>
2026-06-15 13:28:18 -0300

    Creating student service

--- src/main/java/com/jeanhefler/attendence_list/services/StudentService.java
+++ src/main/java/com/jeanhefler/attendence_list/services/StudentService.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Service;
 
 import com.jeanhefler.attendence_list.dtos.StudentDto;
 import com.jeanhefler.attendence_list.entities.Student;
+import com.jeanhefler.attendence_list.mappers.ClassRoomMapper;
+import com.jeanhefler.attendence_list.mappers.GuardianMapper;
 import com.jeanhefler.attendence_list.mappers.StudentMapper;
 import com.jeanhefler.attendence_list.respositories.StudentRepository;
 
@@ -13,10 +15,20 @@ import com.jeanhefler.attendence_list.respositories.StudentRepository;
 public class StudentService {
     private StudentRepository studentRepository;
     private StudentMapper studentMapper;
+    private GuardianService guardianService;
+    private GuardianMapper guardianMapper;
+    private ClassRoomService classRoomService;
+    private ClassRoomMapper classRoomMapper;
 
-    public StudentService(StudentRepository studentRepository, StudentMapper studentMapper) {
+    public StudentService(StudentRepository studentRepository, StudentMapper studentMapper,
+            GuardianService guardianService, GuardianMapper guardianMapper, ClassRoomService classRoomService,
+            ClassRoomMapper classRoomMapper) {
         this.studentRepository = studentRepository;
         this.studentMapper = studentMapper;
+        this.guardianService = guardianService;
+        this.guardianMapper = guardianMapper;
+        this.classRoomService = classRoomService;
+        this.classRoomMapper = classRoomMapper;
     }
 
     public List<StudentDto> findStudents(){
@@ -31,6 +43,34 @@ public class StudentService {
         .orElseThrow(() -> new RuntimeException("Student not found"));
         StudentDto response = studentMapper.toDto(data);
         return response;
+    }
+
+    public StudentDto insertNewStudent(StudentDto dto){
+        Student student = this.studentMapper.toEntity(dto);
+        Student newStudent = this.studentRepository.save(student);
+        StudentDto response = this.studentMapper.toDto(
+            this.studentRepository.findById(newStudent.getId()).
+            orElseThrow(() -> new RuntimeException("Student not found")));
+        return response;
+    }
+
+    public StudentDto updateStudent(Long id, StudentDto dto){
+        Student student = this.studentMapper.toEntity(dto);
+        if(!dto.name().isEmpty()) student.setName(dto.name());
+        if(dto.isEnrolled() != student.isEnrolled()) student.setEnrolled(dto.isEnrolled());
+        if(dto.guardianId() != null) student.setGuardian(this.guardianMapper.
+            toEntity(this.guardianService.findGuardianById(dto.guardianId())));
+        if(dto.classroomId() != null) student.setClassRoom(this.classRoomMapper.
+            toEntity(this.classRoomService.findClassRoomById(dto.classroomId())));
+        StudentDto response = this.studentMapper.toDto(this.studentRepository.findById(student.getId())
+        .orElseThrow(() -> new RuntimeException("Student not found")));
+        return response;
+    }
+
+    public void deleteStudent(Long id){
+        Student student = this.studentRepository.findById(id).
+        orElseThrow(() -> new RuntimeException("Student not found"));
+        this.studentRepository.delete(student);
     }
 
 }
