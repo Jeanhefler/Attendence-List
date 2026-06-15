45c94101f42a991cbef86169d8880d094a5a1b72
Jeanhefler <jeanhefler@outlook.com>
2026-06-15 16:45:56 -0300

    Creating student controller

--- src/main/java/com/jeanhefler/attendence_list/controllers/StudentController.java
+++ src/main/java/com/jeanhefler/attendence_list/controllers/StudentController.java
@@ -3,7 +3,12 @@ package com.jeanhefler.attendence_list.controllers;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +29,30 @@ public class StudentController {
     public ResponseEntity<List<StudentDto>> getAll(){
         List<StudentDto> students = studentService.findStudents();
         return ResponseEntity.ok(students);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<StudentDto> getById(@PathVariable Long id){
+        StudentDto response = this.studentService.findStudentById(id);
+        return ResponseEntity.ok().body(response);
+    }
+
+    @PostMapping()
+    public ResponseEntity<StudentDto> insert(@RequestBody StudentDto dto){
+        StudentDto response = this.studentService.insertNewStudent(dto);
+        return ResponseEntity.ok().body(response);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<StudentDto> update(@PathVariable Long id, @RequestBody StudentDto dto){
+        StudentDto response = this.studentService.updateStudent(id, dto);
+        return ResponseEntity.ok().body(response);
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<StudentDto> delete(@PathVariable Long id){
+        StudentDto student = this.studentService.findStudentById(id);
+        this.studentService.deleteStudent(student.id());
+        return ResponseEntity.noContent().build();
     }
 }
